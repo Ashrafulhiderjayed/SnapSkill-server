0c0f2b4aec1e5c29feeb0fc8e9b3c5a809159806
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2023-06-16 11:28:13 +0600

    get myclass section

--- index.js
+++ index.js
@@ -52,6 +52,12 @@ async function run() {
         res.send(result);
       });
 
+      app.get("/myClasses", async (req, res) => {
+        const query = { instructor_email: req.query.email };
+        const result = await classesCollection.find(query).toArray();
+        res.send(result);
+      });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
