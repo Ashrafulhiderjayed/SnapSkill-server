e8e02dd69b31ba1a81763ca3238344fb449f86e9
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2023-06-16 14:41:32 +0600

    remove .env

--- index.js
+++ index.js
@@ -339,12 +339,11 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-
-
+// 
+// 
 
 app.get('/', (req, res) => { 
-  res.send('HSummer Photo Camp Server is running!');
+  res.send('Summer Photo Camp Server is running!');
 })
 
 
