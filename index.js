9887d406e6d64181d5519a81e29104f5bd80daf5
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2023-06-16 11:33:06 +0600

    make instructor

--- index.js
+++ index.js
@@ -88,6 +88,19 @@ async function run() {
         res.send(result);
       });
 
+      //make instructor
+    app.patch("/users/instructor/:id", async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            role: "instructor",
+          },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
